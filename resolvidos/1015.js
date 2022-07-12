cccde973aa8c1ded702f8aae75071690c8c07c24
BrandtBre <breno.b1@unochapeco.edu.br>
2022-07-12 18:27:14 -0300

    exercicios novos

---
+++ resolvidos/1015.js
@@ -0,0 +1,11 @@
+var input = require('fs').readFileSync('./dev/stdin.txt', 'utf8');
+var lines = input.split('\n');
+
+const pegarValores = (line) => line.split(" ").map(a => Number(a));
+
+let [x1, y1] = pegarValores(lines.shift());
+let [x2, y2] = pegarValores(lines.shift());
+
+let calculo = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+console.log(calculo.toFixed(4));
